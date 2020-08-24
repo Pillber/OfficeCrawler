82730a54142d856639d7987f0ab5b147ae8e0f49
D Redheendran <melcard16@gmail.com>
2020-08-24 14:40:06 -0500

    Add project files.

---
+++ OfficeCrawler/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OfficeCrawler {
+    public static class Program {
+        [STAThread]
+        static void Main() {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
