dad2db3c42cc4e4cd49444a5f74f7d49e14a907f
D Redheendran <melcard16@gmail.com>
2020-09-04 17:43:54 -0500

    Player class and Movement and Stuff

---
+++ OfficeCrawler/Player.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+
+namespace OfficeCrawler {
+    class Player {
+
+        private Texture2D sprite;
+        private Vector2 pos;
+        private readonly float moveSpeed = 5;
+        public bool moving;
+        public string currentInsult;
+        private SpriteFont insultFont;
+        private MouseState previousMouseState;
+        private KeyboardState previousKeyboardState;
+
+        public Player(Texture2D sprite, Vector2 pos) {
+            this.sprite = sprite;
+            this.pos = pos;
+            moving = true;
+            currentInsult = "no u";
+        }
+
+        public void setTexture(Texture2D newTex) {
+            this.sprite = newTex;
+        }
+
+        public void addFont(SpriteFont font) {
+            this.insultFont = font;
+        }
+
+        public void draw(GameTime gameTime, SpriteBatch spriteBatch) {
+            spriteBatch.Draw(sprite, pos, Color.White);
+            if(!moving) {
+                spriteBatch.DrawString(insultFont, currentInsult, Vector2.Zero, Color.Black);
+            }
+        }
+
+        public void update(GameTime gameTime) {
+            getKeyBoardInput(gameTime);
+            getMouseInput();
+        }
+
+        private void getKeyBoardInput(GameTime gameTime) {
+            KeyboardState keyState = Keyboard.GetState();
+            if (moving) {
+                if (keyState.IsKeyDown(Keys.W)) {
+                    pos.Y -= moveSpeed;
+                }
+                if (keyState.IsKeyDown(Keys.A)) {
+                    pos.X -= moveSpeed;
+                }
+                if (keyState.IsKeyDown(Keys.S)) {
+                    pos.Y += moveSpeed;
+                }
+                if (keyState.IsKeyDown(Keys.D)) {
+                    pos.X += moveSpeed;
+                }
+            } else {
+                Keys[] enteredKeys = keyState.GetPressedKeys();
+                if(enteredKeys.Length > 0) {
+                    Keys enteredKey = keyState.GetPressedKeys()[0];
+                    if (enteredKey == Keys.Back) {
+                        if(currentInsult.Length > 1) {
+                            currentInsult = currentInsult.Substring(0, currentInsult.Length - 2);
+                        }
+                    } else if (enteredKey == Keys.Space) {
+                        currentInsult += " ";
+                    } else {
+                        currentInsult += enteredKey.ToString().ToLower();
+                    }
+                }
+            }
+            previousKeyboardState = keyState;
+        }
+
+        private void getMouseInput() {
+            MouseState mouseState = Mouse.GetState();
+            if(mouseState.RightButton == ButtonState.Pressed && previousMouseState.RightButton != ButtonState.Pressed) {
+                moving = !moving;
+            }
+            previousMouseState = mouseState;
+        }
+
+    }
+}
