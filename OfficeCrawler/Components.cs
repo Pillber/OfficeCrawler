7f2589f50fb3781fda47601da0eb1427e1c4a720
D Redheendran <melcard16@gmail.com>
2020-10-29 09:05:09 -0500

    Planning written in Components.cs. Old Entity system removed.

--- OfficeCrawler/Components.cs
+++ OfficeCrawler/Components.cs
@@ -1,41 +1,54 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 
 
 namespace OfficeCrawler {
     
     /*
-     * Drawing
-     *  -Texture, positioning
-     * positioning
-     *  -x and y, depth/layer?
-     * movement
-     *  -velocity handling, positioning
+     * Sprite Renderer
+     *  -Implement IRenderer
+     *  -Sprite
+     *      -Texture
+     *      -Tint
+     *      -Animation and Animation state
+     *  -Draw
+     *  -Transform
+     *      -Reference to transform on entity, (0, 0) if null
+     * Renderer Interface
+     *  -virtual (abstract) Draw();
+     * UI Renderer
+     *  -Implements IRenderer
+     *  -Blah, Blah, Blah
+     * Transform (has to be object)
+     *  -Position
+     *  -Rotation
+     *  -Scale
+     * Physics
+     *  -Collider
+     *  -Update
+     *  -Calculate collisions
+     * InputListener - Interface/Abstract Class?
+     *  -Switch inputlisteners depending on game state?
+     *  -Listen for changes in the InputManager
+     *  -Delegate methods for keydown, keyup, keyheld, ect.
+     *  -Entity Inplements InputListener
+     *      -InputLIstner.Silence to turn off input listener.
+     * InputManager
+     *  -Wrappings for Keyboard and possibly controller inputs
+     *  -Event-Based
+     *  
+     * 
+     * Entities will have references to each of them. access with "Player.SpriteRender.Draw();", "Enemy.Physics.DisableCollider = true;", "Player.SpriteRenderer.Sprite.Tint = Color.Red;"
+     * 
+     * public class BaseEntity {
+     *  public SpriteRenderer SpriteRenderer;
+     *  ...
+     * }
+     * 
+     * 
+     * 
      */
 
-    public class TransformComponent {
-        Vector2 position;
-        int layer;
 
-
-    }
-   
-
-    public class MovementComponent {
-        Vector2 velocity;
-        Vector2 position;
-    }
-
-    public class DrawingComponent {
-        TransformComponent transform;
-        Texture2D texture;
-
-        public void Draw(SpriteBatch spriteBatch) {
-
-        }
-    }
 }
